a4ea70cf3d2ca116127fab6482e305b6f7976f69
WMnyaka <wandilemnyaka@gmail.com>
2023-06-15 11:17:34 -0700

    Quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,16 +1,16 @@
 #include <unistd.h>
 #include <string.h>
 
-int main(void) 
+int main(void)
 {
 	const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-        ssize_t bytes_written = write(STDERR_FILENO, message, strlen(message));
-    
-        if (bytes_written == -1) 
+	ssize_t bytes_written = write(STDERR_FILENO, message, strlen(message));
+
+	if (bytes_written == -1)
 	{
-        /* Error occurred while writing */
-        return 1;
-    }
-    
-        return 1;
+		/* Error occurred while writing */
+		return 1;
+	}
+
+	return 1;
 }
