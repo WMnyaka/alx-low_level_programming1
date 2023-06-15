f12756c4997ae9d4356496315a7f45c469514c23
WMnyaka <wandilemnyaka@gmail.com>
2023-06-15 08:40:27 -0700

    Puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.n>
+/**
+ * main - A program that prints a line with puts funtion
+ * Return:0 (Success)
+*/
+int main(void)
+{
+	puts("\"Programming is like building multilingual puzzle");
+	return (0);
+}
