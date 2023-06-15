f1bec8e8ea2ebc69016de3cf4ab55d330db17f65
WMnyaka <wandilemnyaka@gmail.com>
2023-06-15 11:04:51 -0700

    Quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+int main(void)
+{
+    const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+    ssize_t bytes_written = write(STDERR_FILENO, message, strlen(message));
+    
+    if (bytes_written == -1) {
+        // Error occurred while writing
+        return 1;
+    }
+    
+    return 1;
+}
