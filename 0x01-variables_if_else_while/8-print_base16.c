710410d0f89edd39346dde4d172487fd8fc50c73
WMnyaka <wandilemnyaka@gmail.com>
2023-06-20 03:16:23 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -3,14 +3,17 @@
 #include <time.h>
 
 int main(void)
-{
-    char low;
 
-    for (low = 'z'; low >= 'a'; low--)
-    {
-        putchar(low);
-    }
-    putchar('\n');
+{
+	int d;
+	char low;
+
+	for (d = '0'; d <= '9'; d++)
+	putchar(d);
+
+	for (low = 'a'; low <= 'f'; low++)
+	putchar(low);
+	putchar('\n');
 
     return 0;
 }
