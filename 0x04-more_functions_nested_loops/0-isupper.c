01d0017aacef7711569e17de26f6eab252f0fac8
WMnyaka <wandilemnyaka@gmail.com>
2023-06-23 03:17:05 -0700

    101-print_number.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,17 +1,16 @@
 #include "main.h"
 
 /**
- * _isupper - Check if a letter is upper
- * @x: The number to be checked
- * Return: 1 for upper letter or 0 for anything else
+ * _isupper -  checks for uppercase character.
+ * @c: c -  Variable
+ * Return: Always 0.
  */
-
-int_isupper(int x)
-
+int _isupper(int c)
 {
-	if (x >= 65 && x <= 90)
+
+	if (c >= 65 && c <= 90)
 	{
-	return (1);
+		return (1);
 	}
 	return (0);
 }
