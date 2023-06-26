a1e74597dcaef459ff1e292ba435585152ed598d
WMnyaka <wandilemnyaka@gmail.com>
2023-06-26 06:10:11 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts - checks the code for the string
+ * @str: validate the character
+ * Return: Always 0.
+ */
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0' ; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
